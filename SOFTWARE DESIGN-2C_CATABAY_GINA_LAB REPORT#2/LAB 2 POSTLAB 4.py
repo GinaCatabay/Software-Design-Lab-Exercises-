4171e289e54be1bdcdb1e83be9ae369bf9ccd393
GinaCatabay <106722412+GinaCatabay@users.noreply.github.com>
2022-06-02 22:42:54 +0800

    Add files via upload

---
+++ SOFTWARE DESIGN-2C_CATABAY_GINA_LAB REPORT#2/LAB 2 POSTLAB 4.py
@@ -0,0 +1,21 @@
+sav_bal = 10.00
+cur_bal = 6532.00
+def_pin = 6969
+
+for _ in range(3):
+    inp = int(input("Enter Your Pin: "))
+    if inp==def_pin:
+        acc_type = input("Please enter c for checking, S for saving: ")
+        if acc_type=='c':
+            print("Your checking balance is: ${}".format(cur_bal))
+            break
+        else:
+            if acc_type=='s':
+                print("Your saving balance is: ${}".format(sav_bal))
+                break
+            else:
+                print("Something wrong")
+                break
+else:
+    print("Your bank card is blocked")
+
